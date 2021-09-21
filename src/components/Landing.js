cc7597f84eecf671e7f58fb9a4d440118ebc1538
Paula <paularamirezsot@gmail.com>
2021-09-21 12:41:05 -0300

    pictures en src

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Button from '@restart/ui/esm/Button';
+
 
 export const Landing = () => {
   return (
     <div>
     <h2> Mesero o Cocinero</h2>
-    
+    <Button variant="outline-primary">Primary</Button>{' '}
+   
+
   </div>
   )
 }
