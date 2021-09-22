da9503e64d354dda5a35811a0f15043ec46c49d2
merimardones <mgmardones@uc.cl>
2021-09-21 22:23:27 -0300

    Attempt to change bg color

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import { Link } from 'react-router-dom';
+import landing from '../components/landing.css'
+
+
 
 export const Landing = () => {
   return (
-    <div>
-    <h2> HOLA PAULA</h2>
-  </div>
+    <Fragment>
+        <div className = "container">
+            <img src = {} alt = "variablehamburguesa" className = "logo"/>
+                <div className="selectWorker">
+                    
+                    <Link className="btn-primary" to="/tableorders">Mesero</Link>
+                   {/* <button className = 'btn-secondary' disabled> Cocina</button>  */}
+                </div>
+        </div>
+        </Fragment>
+    
+      
+            
+   
+  
   )
 }
